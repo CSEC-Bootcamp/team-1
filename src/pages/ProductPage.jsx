c55336f9eea179dfbfbed973d50283a8cd1527cb
Tnsae <tinsaemk6679@gmail.com>
2025-05-05 19:28:15 +0300

    added a stock option

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -8,15 +8,14 @@ export default function ProductPage() {
       "https://media.istockphoto.com/id/1656300567/photo/tea-cup-with-teabag-teatime-creative-concepts.webp?a=1&b=1&s=612x612&w=0&k=20&c=sHsRYAoV-j_49x9JGB3NqOBGgzDJj0_hpX0qbuBzcOo=",
     catagory: "Home Utilities",
     price: "250",
-    Stock: 7,
+    Stock: 4,
     slug: "tea-cup",
   };
   return (
-    <div>
-      <h1 className="flex font-bold text-2xl mb-2 text-red-600">
+    <div className="px-4 md:px-8 lg:px-16">
+      <h1 className="flex font-bold text-2xl mb-2 text-red-600 px-4 md:px-8 lg:px-5">
         Explore our products
       </h1>
-
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full px-3 sm:px-1 md:px-3 lg:px-5">
         <ProductCard product={product} />
         <ProductCard product={product} />
