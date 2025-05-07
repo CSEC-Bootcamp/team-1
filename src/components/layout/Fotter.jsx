7b1e2eb371b01adcf1084a1d4595fdb37308f5d8
Tnsae <tinsaemk6679@gmail.com>
2025-05-07 14:04:00 +0300

    added a navbar

--- src/components/layout/Fotter.jsx
+++ src/components/layout/Fotter.jsx
@@ -1,46 +1,48 @@
-import { FacebookIcon, InstagramIcon, LinkedinIcon, TwitterIcon } from 'lucide-react'
-import React from 'react'
+import {
+  Copyright,
+  FacebookIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  TwitterIcon,
+} from "lucide-react";
+import React from "react";
 
 function Fotter() {
   return (
-    <div className='text-neutral-50 text-base font-normal font-Poppins not-first:leading-normal'>
-        
+    <div className="bg-gray-700 text-neutral-50 text-base font-normal font-Poppins p-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        {/* Column 1 */}
         <div>
-            <h1>Subscribe</h1>
-            <p></p>
-            <input type="text" />
+          <h1 className="text-lg font-bold mb-2">Follow us on social media:</h1>
+          <div className="flex gap-4">
+            <FacebookIcon />
+            <InstagramIcon />
+            <TwitterIcon />
+            <LinkedinIcon />
+          </div>
         </div>
         <div>
-            <h1>Support</h1>
-            <p></p>
-            <p>dev@gmail.com</p>
-            <p></p>
-            <p></p>
-
+          <h1 className="text-lg font-bold mb-2">Subscribe</h1>
+          <p>Get 10% off on your first cart.</p>
         </div>
         <div>
-        <h1>Support</h1>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+          <h1 className="text-lg font-bold mb-2">Support</h1>
+          <p>Email: YigzuSupport@gmail.com</p>
         </div>
         <div>
-        <h1>Support</h1>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+          <h1 className="text-lg font-bold mb-2">Contact us:</h1>
+          <p>Phone: +251-923-445-687</p>
         </div>
-        <div>
-        <h1>Follow us on socials </h1>
-            <FacebookIcon/>
-            <TwitterIcon/>
-            <InstagramIcon/>
-            <LinkedinIcon/>
-        </div>
+      </div>
+      <div>
+        <hr className="border-t border-gray-300 my-8" />
+        <p className="text-gray-100 ">
+          <Copyright />
+          copyright Rimel 2025. All rights reserved.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Fotter
+export default Fotter;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,16 +2,17 @@ import Navbar from "../components/layout/Navbar";
 import Home from "../pages/Home";
 import ProductPage from "../pages/ProductPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Fotter from "../components/layout/Fotter";
 
 export default function RouteLayout() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        
         <Route path="/products" element={<ProductPage />} />
         <Route path="/" element={<Home />} />
       </Routes>
+      <Fotter />
     </BrowserRouter>
   );
 }
